789c6442e839babf5c373377a69f398de3745cce
Anne Edmondson <edmondson.anne@gmail.com>
2023-04-28 21:01:29 -0600

    Commit updates for solutions 1-5

--- week4/week_4.js
+++ week4/week_4.js
@@ -2,22 +2,48 @@
 // 2023-03-30-fe-mountain
 // Week 4 Coding Assignment
 
-// Solution for #1
+// SOLUTION FOR #1
 
-// Create ages array
+// Creating ages array
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]
 
-// Function to substract first element of the array from the last
-let subtractAge = function (ages) {
-    return (ages[ages.length - 1]) - ages[0]
+// Function to substract first element of the ages array from the last
+let subtractAge = function (array) {
+    return (array[array.length - 1]) - array[0]
 }
 console.log(subtractAge(ages))
 
-// Adding new value to array to test dynamic function
+// Adding new value to ages array using the array.push method and re-calling the subtractAge function to confirm that it is dynamic
+
 ages.push(111)
 console.log(subtractAge(ages))
 
-// Finding average age in the array by using the array.reduce method to sum all the numbers in the array and then dividing that sum by the number of elements in the array
+// Finding average age in the ages array
+// Using the array.reduce method to sum all the numbers in the array. "x" represents the accumulated sum and "y" represents the current element's value.
+// Dividing that sum by the number of elements in the array
 
 let averageArrayAge = array => array.reduce((x,y) => x + y, 0) / array.length
 console.log(averageArrayAge(ages))
+
+// SOLUTION FOR #2
+
+// Creating names array
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+
+
+// SOLUTION FOR #3
+// array[array.length - 1]
+
+// SOLUTION FOR #4
+// array[0]
+
+// SOLUTION FOR #5
+
+// Creating nameLengths array
+let nameLengths = []
+
+// Creating function to determine the length of each name
+function lengthOfNAme = 
+
+// Calling function on each element in the names array and pushing the resulting value to the new nameLengths array
+names.forEach(lengthOfName)
